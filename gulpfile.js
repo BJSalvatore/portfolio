6f20b28619da8103a2c2c8eca743d43ae43a06c7
Beth <bethsalvatore2018@gmail.com>
2018-08-12 15:57:27 -0400

    adds jumbotron and container with navbar

---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var runSequence = require('run-sequence');
+
+// task to copy all htm/html files from app directory to dist directory
+gulp.task('copyhtml', function() {
+  return gulp.src('app/*.+(htm|html)')
+	.pipe(gulp.dest('dist/'))
+ });
+
+gulp.task('copyimages', function(){
+  return gulp.src('app/images/*.+(png|jpg|gif)')
+  .pipe(gulp.dest('dist/images'))
+});
+
+gulp.task('sass', function() {
+	return gulp.src('app/*.scss')
+		.pipe(sass())
+		.pipe(gulp.dest('dist/'))
+});
+
+// function to change sass code to css
+gulp.task('copycss', function() {
+	return gulp.src('app/*.css')
+		.pipe(gulp.dest('dist/'))
+});
+
+gulp.task('watch', function() {
+	gulp.watch('app/**/*', ['copyhtml', 'copyimages', 'sass', 'copycss'])
+});
+
+gulp.task('default', function(callback) {
+	runSequence(['sass', 'copycss', 'copyhtml'], callback)
+});
