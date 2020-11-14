08824ae346b5dd9990fa4a90f63433f1d0278c65
demoncowgirl <bethsalvatore2018@gmail.com>
2020-11-13 20:16:40 -0600

    first commit for portfolio main page update

--- gulpfile.js
+++ gulpfile.js
@@ -3,8 +3,6 @@ const {
 } = require('gulp');
 
 var gulp = require('gulp');
-// var sass = require('gulp-sass');
-// var runSequence = require('run-sequence');
 
 // task to copy all htm/html files from app directory to dist directory
 function copyhtml(cb) {
@@ -22,11 +20,6 @@ function copycss(cb) {
     .pipe(gulp.dest('dist/css/'))
 }
 
-// function copyjs(cb) {
-//   return gulp.src('app/js/*.js')
-//     .pipe(gulp.dest('dist/js/'))
-// }
-
 // function to change sass code to css
 function sass(cb) {
   return gulp.src('app/scss/*.scss')
@@ -34,22 +27,9 @@ function sass(cb) {
     .pipe(gulp.dest('dist/css/'))
 }
 
-// gulp.task('watch', function() {
-//   gulp.watch('./app/scss/*.scss', gulp.series('sass'));
-//   gulp.watch('./app/*.html', gulp.series('copyhtml'));
-//   gulp.watch('./app/images/*.+(png|jpg|gif)', gulp.series('copyimages'));
-//   gulp.watch('./app/css/*.css', gulp.series('copycss'));
-//   gulp.watch('./app/js/*.js', gulp.series('copyjs'));
-//
-// });
 
 function watch(cb) {
   gulp.watch('app/*', ['copyhtml', 'copyimages', 'copycss', 'copyjs', 'sass'])
 }
-
-// function
-// default (cb) {
-//   runSequence(['sass', 'copyhtml', 'copycss'], callback)
-// });
 
 exports.default = series(copyhtml, copyimages, copycss, sass);
